480ca1e5740103d4bf1f40e1ea0fe8a52f7e4354
dohre2023 <media@dohre.pl>
2024-12-09 20:13:21 +0100

    ver1

---
+++ src/main/java/bookManage/main.java
@@ -0,0 +1,9 @@
+package bookManage;
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("napis");
+    }
+
+}
---
+++ src/test/java/testDohre/firpro.java
@@ -0,0 +1,14 @@
+package testDohre;
+
+import org.junit.Test;
+
+public class firpro {
+    @Test
+    public void setProp(){
+        System.setProperty("","");
+
+
+    }
+
+
+}
